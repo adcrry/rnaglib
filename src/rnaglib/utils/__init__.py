70fdf59f1e46542d77c7d48f3930e1d152e223e0
wisskarrou <wisskarrou@gmail.com>
2026-01-27 10:51:52 +0100

    add RBF encoding

--- src/rnaglib/utils/__init__.py
+++ src/rnaglib/utils/__init__.py
@@ -17,6 +17,7 @@ from .misc import tonumpy
 
 from .task_utils import print_statistics
 from .task_utils import DummyResidueModel, DummyGraphModel
+from .represent_utils import rbf_expand
 
 from .wrappers import rna_align_wrapper, cdhit_wrapper, locarna_wrapper, US_align_wrapper
 
